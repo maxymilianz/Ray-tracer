ffc57c42a161e907375160fe2935abcb62ce4aa0
maxymilianz <289528@uwr.edu.pl>
2018-01-06 15:10:53 +0100

    pixel_to_vector function and Vector module changes

--- main.ml
+++ main.ml
@@ -14,8 +14,12 @@ module type VECTOR = sig
     type t = V of float * float * float
 
     val create : float -> float -> float -> t
+    val add : t -> t -> t
     val displacement : t -> t -> t      (* displacement between 2 points *)
+    val mult : t -> float -> t
     val dist : t -> t -> float
+    val len : t -> float
+    val normalize : t -> t
 end
 
 module Vector : VECTOR = struct
@@ -23,9 +27,20 @@ module Vector : VECTOR = struct
 
     let create x y z = V (x, y, z)
 
+    let add (V (x, y, z)) (V (x', y', z')) = V (x +. x', y +. y', z +. z')
+
     let displacement (V (x, y, z)) (V (x', y', z')) = V (x' -. x, y' -. y, z' -. z)
 
+    let mult (V (x, y, z)) c = V (x *. c, y *. c, z *. c)
+
     let dist (V (x, y, z)) (V (x', y', z')) = let dx = x -. x' and dy = y -. y' and dz = z -. z' in sqrt (dx*.dx +. dy*.dy +. dz*.dz)
+
+    let len v = dist (V (0., 0., 0.)) v
+
+    let normalize v =
+        match v with
+        V (x, y, z) -> let len = len v in
+            V (x /. len, y /. len, z /. len)
 end
 
 module type OBJ = sig
@@ -42,7 +57,13 @@ module Obj : OBJ = struct
     let intersection q w e = Some q
 end
 
-let pixel_to_vector q w e = Vector.create 1. 2. 3. (* TODO *)
+let pixel_to_vector res_x res_y x y canvas_coords =
+    (* canvas_coords is list of upper left, upper right, lower right and lower left Vectors *)
+    match canvas_coords with
+    ul, ur, lr, ll -> let horizontal, vertical = Vector.displacement ul ur, Vector.displacement ul ll in
+        let width, height = Vector.len horizontal, Vector.len vertical in
+        let ratio_x, ratio_y = float x /. float res_x, float y /. float res_y in
+        Vector.(add ul (add (mult horizontal ratio_x) (mult vertical ratio_y)))
 
 let closest_intersection pos v objs =       (* camera pos -> camera dir -> objs -> optional object and point of intersection *)
     let rec aux closest dist = function     (* optional current closest object and point -> dist (not optional; None => infinity) -> same return type as above *)
@@ -59,7 +80,7 @@ let render res_x res_y canvas_coords pos objs bg_color =        (* returns list 
         if y = res_y then []
         else let rec aux_x x =
             if x = res_x then []
-            else let v = Vector.displacement pos (pixel_to_vector x y canvas_coords) in
+            else let v = Vector.displacement pos (pixel_to_vector res_x res_y x y canvas_coords) in
                 match closest_intersection pos v objs with
                     None -> bg_color
                     | Some (obj, point) -> Obj.color pos point obj :: aux_x (x + 1) in
