930865e41bdc9eac29130a05dd3feb84bee2a7f6
maxymilianz <289528@uwr.edu.pl>
2018-01-20 20:44:47 +0100

    Comments in vector.mli

--- vector.mli
+++ vector.mli
@@ -5,8 +5,8 @@ val create : float -> float -> float -> t
 val add : t -> t -> t
 val subtract : t -> t -> t
 val displacement : t -> t -> t
-val mult : t -> float -> t
-val div : t -> float -> t
+val mult : t -> float -> t      (* mult vector * float *)
+val div : t -> float -> t       (* divide vector / float *)
 
 val dot_prod : t -> t -> float
 
@@ -15,8 +15,8 @@ val dist : t -> t -> float
 val len : t -> float
 
 val normalize : t -> t
-val opposite : t -> t
+val opposite : t -> t       (* vec -> -vec *)
 val symmetric : t -> t -> t        (* point to reflect -> normal -> reflected point *)
 
-val angle : t -> t -> float
+val angle : t -> t -> float     (* angle between 2 vectors *)
 val abs_angle : t -> t -> float
