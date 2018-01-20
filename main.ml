844b9808523668f63d6b74628de3309409a972b6
maxymilianz <289528@uwr.edu.pl>
2018-01-20 13:09:49 +0100

    Much of the Parser module

--- main.ml
+++ main.ml
@@ -470,27 +470,114 @@ let test filename =
 module type PARSER = sig
     type t
 
-    val read_file : string -> Scene.t       (* filename -> scene read from file *)
+    val read_file : string -> string       (* filename -> scene read from file *)
 end
 
 module Parser : PARSER = struct
+    open Str
+
     type t = Token_Int of int | Token_Float of float | Token_Vector of Vector.t | Token_Color of Color.t | Token_Sph of Sphere.t | Token_Surf of Surface.t
         | Token_Obj of Obj.t | Token_Light of Light.t
 
+    let int_regexp = regexp "[0-9]+"
+
+    let float_regexp = regexp "[0-9]*\.[0-9]+"
+
+    let objs_regexp, lights_regexp = regexp "OBJS.+ENDOBJS", regexp "LIGHTS.+ENDLIGHTS"
+
+    let sph_regexp, surf_regexp = regexp "Sph", regexp "Surf"
+
+    let point_regexp, sun_regexp = regexp "Point.+EndPoint", regexp "Sun.+EndSun"
+
+    let match_int str pos =     (* string -> pos to start searching -> (found int, new pos to start searching for sth else) *)
+        let int_str = string_match int_regexp str pos; matched_string str in
+        int_of_string int_str, match_end ()
+
+    let match_float str pos =
+        let float_str = string_match float_regexp str pos; matched_string str in
+        float_of_string float_str, match_end ()
+
+    let match_res str pos =
+        let res_x, pos1 = match_int str pos in
+        let res_y, pos2 = match_int str pos1 in
+        (res_x, res_y), pos2
+
+    let match_vector str pos =
+        let x, pos1 = match_float str pos in
+        let y, pos2 = match_float str pos1 in
+        let z, pos3 = match_float str pos2 in
+        Vector.create x y z, pos3
+
+    let match_canvas_coords str pos =
+        let ul, pos1 = match_vector str pos in
+        let ur, pos2 = match_vector str pos1 in
+        let lr, pos3 = match_vector str pos2 in
+        let ll, pos4 = match_vector str pos3 in
+        (ul, ur, lr, ll), pos4
+
+    let match_color str pos =
+        let r, pos1 = match_int str pos in
+        let g, pos2 = match_int str pos1 in
+        let b, pos3 = match_int str pos2 in
+        Color.from_int r g b, pos3
+
+    let match_color_ratio str pos =
+        let glow, pos1 = match_int str pos in
+        let refl, pos2 = match_int str pos1 in
+        let scat, pos3 = match_int str pos2 in
+        (glow, refl, scat), pos3
+
+    let match_camera_pos = match_vector
+
+    let match_rec_depth = match_int
+
+    let match_sph str pos =
+        let center, pos1 = match_vector str pos in
+        let radius, pos2 = match_float str pos1 in
+        let color, pos3 = match_color str pos2 in
+        let color_ratio, pos4 = match_color_ratio str pos3 in
+        Obj.create_sph (Sphere.create center radius color color_ratio), pos4
+
+    let match_sph str pos =
+        let normal, pos1 = match_vector str pos in
+        let point, pos2 = match_float str pos1 in
+        let color, pos3 = match_color str pos2 in
+        let color_ratio, pos4 = match_color_ratio str pos3 in
+        Obj.create_surf (Surface.create normal point color color_ratio), pos4
+
+    let closer_obj str = function
+        None, None -> None
+        | None, Some surf_pos -> Some (match_surf str surf_pos)
+        | Some sph_pos, None -> Some (match_sph str sph_pos)
+        | Some sph_pos, Some surf_pos ->
+            if sph_pos < surf_pos then Some (match_sph str sph_pos)
+            else Some (match_surf str surf_pos)
+
+    let match_obj str pos =
+        let sph_pos = try
+                Some (search_forward sph_regexp str pos)
+            with e ->
+                None
+        and surf_pos = try
+                Some (search_forward surf_regexp str pos)
+            with e ->
+                None in
+        closer_obj str (sph_pos, surf_pos)
+
+    let match_objs str pos =
+        let rec aux objs pos' =
+            match match_obj str pos' with
+            None -> objs, pos'
+            Some (obj, pos'') -> aux (obj :: objs) pos'' in
+        aux [] pos
+
     let read_file filename =
         let stream = open_in filename in
-        try
-            let line = input_line stream in
-            Printf.printf "%s\n" line;
-            let line = input_line stream in
-            Printf.printf "%s\n" line;
-            let line = input_line stream in
-            Printf.printf "%s\n" line;
-            let line = input_line stream in
-            Printf.printf "%s\n" line;
-            flush stdout;
-            close_in stream
-        with e ->
-            close_in_noerr stream;
-            raise e
+        let rec aux str =
+            try
+                aux (str ^ input_line stream)
+            with e ->
+                close_in_noerr stream;
+                str in
+        str_to_scene (aux "")
 end
